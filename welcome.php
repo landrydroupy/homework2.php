e5a2745a4a153d72c49dbc51b4c9333d82cde1f6
Landry Droupy <78325544+landrydroupy@users.noreply.github.com>
2022-09-12 14:20:22 -0500

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -9,8 +9,10 @@
   <body>
     <h1>Thank you for the information!</h1>
     
-    Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
+  Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?><br>
+Your home address is: <?php echo $_POST["email"]; ?>
+   
     
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
