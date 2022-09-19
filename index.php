833650ef4d88f3f0e368a11f9af968c87990e1fa
Landry Droupy <78325544+landrydroupy@users.noreply.github.com>
2022-09-19 10:43:51 -0500

    restart index

--- index.php
+++ index.php
@@ -1,25 +1,35 @@
 
 
     
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-  </body>
-    </div>
-<div>
- <h1>Hello, world!</h1>
-    <form action="header.php" method="get">
+    <!DOCTYPE html>
+<html>
+
+	<head>
+	<?php include("header.php");?>
+	</head>	
+
+<body>
+
+<div class="container" id="main-content">
+	    
+	<h2>Landry Droupy - Homework 2</h2>
+	<p>Filling out information here will use the POST method</p>
+<form action="handlepost.php" method="post">
 Name: <input type="text" name="name"><br>
+	
 E-mail: <input type="text" name="email"><br>
 <input type="submit">
 </form>
-    
-    <a class="btn btn-primary" href="page2.html" role="button">Page 2</a>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-  </body>
-</html>
-     </div>
+
+	<p>Filling out the information here will use the GET method</p>
+<form action="handleget.php" method="get">
+Name: <input type="text" name="name"><br>
+	
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+</div>
 
 
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
-  </body>
+</body>
 </html>
