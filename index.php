332372e008e0672cf90ed7f72347367cae511d18
Landry Droupy <78325544+landrydroupy@users.noreply.github.com>
2022-09-18 19:10:59 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,4 @@
 
-<h1>My website</h1>
-   
-
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -56,7 +53,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
     </div>
-
+<div>
  <h1>Hello, world!</h1>
     <form action="header.php" method="get">
 Name: <input type="text" name="name"><br>
@@ -68,7 +65,7 @@ E-mail: <input type="text" name="email"><br>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
 </html>
-
+     </div>
 
 
 
